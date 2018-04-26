ce279460d84e21e657aa47f282d659a84a152005
Frank Yang <yangpoan@gmail.com>
2018-04-26 15:01:11 +0800

    add cli searchBook

---
+++ bin/searchBook.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const Table = require('tty-table');
+
+const searchBook = require('..')
+
+program
+  .version(require('../package.json').version, '-v --version')
+  .usage('<name> [options]')
+  .option(
+    '-p, --platform <platform>',
+    'Searching book from Google or Book.',
+    /^(Google|Book)$/i,
+    'Book')
+  .option('-l --limit <limit>', 'Maximum number of books to return', /^\d+$/, 10)
+  .option('-i --isbn <isbn>', 'ISBN of the book.')
+  .parse(process.argv)
+
+if (program.isbn === undefined && program.args.length === 0) {
+  console.log('Please input a name of book.')
+}
+
+const options = {
+  field: program.isbn ? 'isbn' : 'title', // Search in a specified field
+  limit: parseInt(program.limit, 10) // coercion string to number
+}
+
+const header = [
+  {
+    alias: '書名',
+    value: 'title',
+    align: 'left',
+    width: 30
+  },
+  {
+    alias: '作者',
+    value: 'authors',
+    align: 'left',
+    width: 30
+  },
+  {
+    alias: '出版社',
+    value: 'publisher',
+    align: 'left',
+    width: 30
+  }
+]
+
+searchBook(program.platform, program.args.join(' '), options)
+  .then((result) => {
+    const books = result.map((book) => {
+      const { title, authors, publisher } = book
+      return {
+        title,
+        authors: book.authors.join(', '),
+        publisher
+      }
+    })
+    const table = Table(header, result)
+    console.log(table.render())
+  })
+  .catch((error) => {
+    console.log(error.toString())
+  })
